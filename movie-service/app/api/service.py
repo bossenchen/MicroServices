1985b0528d3154a9c9ac9cdc1b503d628cf8a3f0
neochen <spony_man@foxmail.com>
2022-06-13 22:17:35 +0800

    修改了拼接的 casts 服务的地址

--- movie-service/app/api/service.py
+++ movie-service/app/api/service.py
@@ -6,7 +6,7 @@ url = os.environ.get('CAST_SERVICE_HOST_URL') or CAST_SERVICE_HOST_URL
 
 
 def is_cast_present(cast_id: int) -> bool:
-    r = httpx.get(f'{url}{cast_id}/')
+    r = httpx.get(f'{url}{cast_id}')
     return True if r.status_code == 200 else False
 
 
