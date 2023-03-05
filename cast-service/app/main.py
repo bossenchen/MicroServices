3dd4a05af7b42d080d89edd3cb3970a4b47848c6
neochen <spony_man@foxmail.com>
2023-03-06 03:29:25 +0800

    Add CORSMiddleware

--- cast-service/app/main.py
+++ cast-service/app/main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
 from .api.casts import casts
 from .api.db import metadata, database, engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 metadata.create_all(engine)
 
 app = FastAPI(
     openapi_url="/api/v1/casts/openapi.json",
     docs_url="/api/v1/casts/docs"
+)
+
+origins = [
+    'http://localhost:*',
+    'https://localhost:*'
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[
+        '*'
+    ],
+    allow_headers=[
+        '*'
+    ]
 )
 
 
--- movie-service/app/main.py
+++ movie-service/app/main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
 from .api.movies import movies
 from .api.db import metadata, database, engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 metadata.create_all(engine)
 
 app = FastAPI(
     openapi_url="/api/v1/movies/openapi.json",
     docs_url="/api/v1/movies/docs"
+)
+
+origins = [
+    'http://localhost:*',
+    'https://localhost:*'
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[
+        '*'
+    ],
+    allow_headers=[
+        '*'
+    ]
 )
 
 
